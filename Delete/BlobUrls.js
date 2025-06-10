e9a3928cdd851f374af3c3d82272a33d304e6fb0
Vinoth <118555830+Vinoth82003@users.noreply.github.com>
2025-06-10 21:27:58 +0530

    Schema updated

--- Delete/BlobUrls.js
+++ Delete/BlobUrls.js
@@ -1,16 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const blobUrlSchema = new mongoose.Schema({
   code: {
     type: String,
     required: true,
     unique: true,
-    length: 4
+    length: 4,
   },
-  blobUrl: String,
-  expireTime: {type: Date, default: Date.now},
-  createdAt: { type: Date, default: Date.now, }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  expireTime: {
+    type: Date,
+    default: function () {
+      return new Date(Date.now() + 5 * 60 * 1000); // 5 minutes from now
+    },
+  },
 });
 
-const BlobUrl = mongoose.models.BlobUrl || mongoose.model('BlobUrl', blobUrlSchema);
+const BlobUrl =
+  mongoose.models.BlobUrl || mongoose.model("BlobUrl", blobUrlSchema);
 export default BlobUrl;
